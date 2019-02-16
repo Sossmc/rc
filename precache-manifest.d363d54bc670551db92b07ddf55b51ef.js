4f4f464de296a54c66928e1f1169e2f0762b6a89
sossmc <sossmc@outlook.com>
2019-02-16 19:43:21 +0800

    Updates

---
+++ precache-manifest.d363d54bc670551db92b07ddf55b51ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b8c6621ebb2e9d3047f",
+    "url": "/static/css/main.401315d4.chunk.css"
+  },
+  {
+    "revision": "3b8c6621ebb2e9d3047f",
+    "url": "/static/js/main.3b8c6621.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "43e86961aa90a90d4071",
+    "url": "/static/css/2.7b20d606.chunk.css"
+  },
+  {
+    "revision": "43e86961aa90a90d4071",
+    "url": "/static/js/2.43e86961.chunk.js"
+  },
+  {
+    "revision": "f5f339ef4c62fa56a50673b0adc2eacd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76d3ccd5204bfbf54065a800cec6ded4.js"
+  "/precache-manifest.d363d54bc670551db92b07ddf55b51ef.js"
 );
 
 workbox.clientsClaim();
