cd6077b348c330eda5b23c61f6601d2071ca1f0e
sossmc <sossmc@outlook.com>
2019-02-17 11:52:11 +0800

    Updates

---
+++ precache-manifest.8d66370a002e1c6ae973896dcb84c827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "515534f86e84467d1eaa",
+    "url": "./static/css/main.401315d4.chunk.css"
+  },
+  {
+    "revision": "515534f86e84467d1eaa",
+    "url": "./static/js/main.515534f8.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "13f4583c1ed4e46448ed",
+    "url": "./static/css/2.6f8e1aad.chunk.css"
+  },
+  {
+    "revision": "13f4583c1ed4e46448ed",
+    "url": "./static/js/2.13f4583c.chunk.js"
+  },
+  {
+    "revision": "6a1e9a88eee63ab01f3965ab08b3e9d8",
+    "url": "./index.html"
+  }
+];
